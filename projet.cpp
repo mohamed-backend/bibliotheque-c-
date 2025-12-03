139111a44e18b5531b62e97ebe08dd217226ebe5
kimaro <houssamsalek0707@gmail.com>
2025-12-03 11:16:18 +0100

    Update projet.cpp

--- projet.cpp
+++ projet.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <memory>
 #include <string>
 using namespace std;
 
-class media
-{
+class media {
 protected:
     int id;
     string titre;
@@ -12,20 +10,13 @@ protected:
 
 public:
     media(int h, string w, bool j) : id(h), titre(w), dispo(j) {}
-
-    virtual ~media()
-    {
-        cout << "deleted" << endl;
-        
-        
-    }
-
-    virtual void afficher()  = 0;
+    virtual ~media() { cout << "deleted" << endl; }
+    virtual void afficher() = 0;
 };
 
-class livre : public media
-{
-private:
+// ===== LIVRE =====
+class livre : public virtual media {
+protected:
     string auteur;
     int Npage;
 
@@ -33,71 +24,89 @@ public:
     livre(int id, string titre, bool dispo, string auteur, int Npage)
         : media(id, titre, dispo), auteur(auteur), Npage(Npage) {}
 
-    void afficher()  override
-    {
-        cout << "Le livre \"" << titre
-             << "\" de l'auteur " << auteur
-             << " contient " << Npage << " pages."
-             << endl;
+    void afficher() override {
+        cout << "Livre: " << titre 
+             << ", Auteur: " << auteur
+             << ", Pages: " << Npage << endl;
     }
 };
-class video : public media
-{
+
+// ===== VIDEO =====
+class video : public media {
 private:
     int dure;
     string qualit;
 
 public:
-    video(int id, string titre, bool dispo, int dure, string qualit) 
-    : media(id, titre, dispo), dure(dure), qualit(qualit) {}
-    void afficher()  override
-    {
-        cout << "Le video \"" << titre
-             << "\" de dure " << dure 
-             << " est de qualite " << qualit<< endl;
+    video(int id, string titre, bool dispo, int dure, string qualit)
+        : media(id, titre, dispo), dure(dure), qualit(qualit) {}
+
+    void afficher() override {
+        cout << "Video: " << titre 
+             << ", Durée: " << dure 
+             << ", Qualité: " << qualit << endl;
     }
 };
-class audio : public media {
-private:
+
+// ===== AUDIO =====
+class audio : public virtual media {
+protected:
     string publicateur;
     int duree;
-public:    
-    audio(int t,  string p, bool a,  string art, int d)
-        : media(t, p, a), publicateur(art), duree(d) {}
-    void afficher() override {
-        cout << "Audio: " << titre <<  ", publie par " << publicateur <<  ", duree: " << duree << endl;
-    }
-};
-class audiobook: public livre, public audio {
+
 public:
-    audiobook(int id, string titre, bool dispo, string auteur, int Npage, string publicateur, int duree)
-        : livre(id, titre, dispo, auteur, Npage), audio(id, titre, dispo, publicateur, duree) {}
+    audio(int id, string titre, bool dispo, string publicateur, int duree)
+        : media(id, titre, dispo), publicateur(publicateur), duree(duree) {}
 
     void afficher() override {
-        livre::afficher();
-        audio::afficher();
+        cout << "Audio: " << titre 
+             << ", Publié par: " << publicateur
+             << ", Durée: " << duree << endl;
     }
 };
 
-int main()
-{
-    cout << "========TEST========="<<endl;
-    cout << "---- TEST LIVRE ----" << endl;
+// ===== AUDIOBOOK =====
+class audiobook : public livre, public audio {
+public:
+    audiobook(int id, string titre, bool dispo,
+              string auteur, int Npage,
+              string publicateur, int duree)
+        : media(id, titre, dispo),   // call media ONCE
+          livre(id, titre, dispo, auteur, Npage),
+          audio(id, titre, dispo, publicateur, duree) {}
+
+    void afficher() override {
+        cout << "AUDIOBOOK" << endl;
+        cout << "- Titre: " << titre << endl;
+        cout << "- Auteur: " << auteur << endl;
+        cout << "- Pages: " << Npage << endl;
+        cout << "- Publié par: " << publicateur << endl;
+        cout << "- Durée Audio: " << duree << endl;
+    }
+};
+
+int main() {
+    cout << "========TEST=========\n";
+
+    cout << "---- TEST LIVRE ----\n";
     media* m1 = new livre(1, "Harry Potter", true, "J.K. Rowling", 350);
     m1->afficher();
     delete m1;
 
-    cout << "\n---- TEST VIDEO ----" << endl;
+    cout << "\n---- TEST VIDEO ----\n";
     media* m2 = new video(2, "Avengers", true, 120, "4K");
     m2->afficher();
     delete m2;
 
-    cout << "\n---- TEST AUDIO ----" << endl;
+    cout << "\n---- TEST AUDIO ----\n";
     media* m3 = new audio(3, "Podcast Tech", true, "Houssam Studio", 45);
     m3->afficher();
     delete m3;
 
-    cout << "\n---- FIN DES TESTS ----" << endl;
-system("pause");    
+    cout << "\n---- TEST AUDIOBOOK ----\n";
+    media* m4 = new audiobook(4, "LOTR", true, "Tolkien", 500, "StudioX", 120);
+    m4->afficher();
+    delete m4;
+
     return 0;
 }
